56957c6916b0f9a49fed6b4361d9b18a694f41d1
cho1n <cjaewon0217@naver.com>
2023-07-02 16:52:27 +0900

    FEAT: 모니터링 페이지 초안

--- src/pages/Monitoring/Monitor.tsx
+++ src/pages/Monitoring/Monitor.tsx
@@ -6,12 +6,66 @@ import { useNavigate } from "react-router-dom";
 
 
 function Monitor() {
+    const navigate = useNavigate();
+
+    const goToMain = () => {
+        navigate("/");
+    };
+
+    const goToBack = () => {
+        navigate(-1);
+    };
 
     return (
         <div>
             <div className="text-block">
-            <p className="shocontainername">우와요기이츠라이더스</p>
+                <p className="container-name">우와요기이츠라이더스</p>
             </div>
+
+            <div className="cpu-block">
+                <div className="NameBlock">
+                    <p className="text-match">CPU Utilization</p>
+                </div>
+            </div>
+
+            <div className="log-block">
+                <div className="NameBlock">
+                    <p className="text-match">Log Check</p>
+                </div>
+            </div>
+
+            <div className="memory-block">
+                <div className="NameBlock">
+                    <p className="text-match">Memory</p>
+                </div>
+            </div>
+
+            <div className="disk-block">
+                <div className="NameBlock">
+                    <p className="text-match">Disk I/O</p>
+                </div>
+            </div>
+
+            <div className="networkin-block">
+                <div className="NameBlock">
+                    <p className="text-match">Network In</p>
+                </div>
+            </div>
+
+            <div className="networkout-block">
+                <div className="NameBlock">
+                    <p className="text-match">Network Out</p>
+                </div>
+            </div>
+
+            <button className="home-button" onClick={goToMain}>
+                <p className="button-text">홈으로</p>
+            </button>
+
+            <button className="back-button" onClick={goToBack}>
+                <p className="button-text">이전으로</p>
+            </button>
+
         </div>
       
     );

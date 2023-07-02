46c169f6f7b02d684201454ef34197dd57c1a0f8
cho1n <cjaewon0217@naver.com>
2023-07-02 19:42:09 +0900

    FEAT: 모니터링 그래프 추가

--- src/pages/Monitoring/Monitor.tsx
+++ src/pages/Monitoring/Monitor.tsx
@@ -1,8 +1,12 @@
 import React from "react";
 import '../../assets/css/monitoringPage.css';
 import { useNavigate } from "react-router-dom";
-
-
+import CPUChart from "../../components/CpuChart";
+import LogChart from "../../components/LogChart";
+import MemoryChart from "../../components/MemoryChart";
+import DiskChart from "../../components/DiskChart";
+import NetworkInChart from "../../components/NetworkInChart";
+import NetworkOutChart from "../../components/NetworkOutChart";
 
 
 function Monitor() {
@@ -26,36 +30,42 @@ function Monitor() {
                 <div className="NameBlock">
                     <p className="text-match">CPU Utilization</p>
                 </div>
+                <CPUChart />
             </div>
 
             <div className="log-block">
                 <div className="NameBlock">
                     <p className="text-match">Log Check</p>
                 </div>
+                <LogChart />
             </div>
 
             <div className="memory-block">
                 <div className="NameBlock">
                     <p className="text-match">Memory</p>
                 </div>
+                <MemoryChart />
             </div>
 
             <div className="disk-block">
                 <div className="NameBlock">
                     <p className="text-match">Disk I/O</p>
                 </div>
+                <DiskChart />
             </div>
 
             <div className="networkin-block">
                 <div className="NameBlock">
                     <p className="text-match">Network In</p>
                 </div>
+                <NetworkInChart />
             </div>
 
             <div className="networkout-block">
                 <div className="NameBlock">
                     <p className="text-match">Network Out</p>
                 </div>
+                <NetworkOutChart />
             </div>
 
             <button className="home-button" onClick={goToMain}>
@@ -69,6 +79,6 @@ function Monitor() {
         </div>
       
     );
-}
+};
 
 export default Monitor;

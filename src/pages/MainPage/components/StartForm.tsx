9b98673f058c84c0092a9191a4b9af3dfbf7fc34
minwoo0419 <ghdl464@naver.com>
2023-07-09 21:19:09 +0900

    Feat : 버튼액션추가

--- src/pages/MainPage/components/StartForm.tsx
+++ src/pages/MainPage/components/StartForm.tsx
@@ -1,4 +1,4 @@
-import React, {useState, useEffect} from "react";
+import React, {useState} from "react";
 import '../../../assets/css/button.css';
 import { useNavigate } from "react-router-dom";
 

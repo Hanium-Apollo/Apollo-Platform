6f7eb6c406fdfd311c4c4e5fd6e8a92884fe5bdf
Yun-Changseop <akakertcobain@naver.com>
2023-07-24 15:47:56 +0900

    Update WaitPage.tsx

--- src/pages/Wait/WaitPage.tsx
+++ src/pages/Wait/WaitPage.tsx
@@ -1,50 +1,55 @@
-import { useNavigate } from 'react-router-dom';
-import React from 'react';
-import { Button } from '@mui/material';
-import axios from 'axios';
+import { useNavigate } from "react-router-dom";
+import React from "react";
+import { Button } from "@mui/material";
+import axios from "axios";
 
 interface WaitProps {
-    userInfo?: Partial<UserInfo>;
+  userInfo?: Partial<UserInfo>;
 }
 
 interface UserInfo {
-    id: string; 
-    login: string;
-    username: string;
-    email: string;
-    profileUrl: string;
+  id: string;
+  login: string;
+  username: string;
+  email: string;
+  profileUrl: string;
 }
 
 const Wait: React.FC<WaitProps> = ({ userInfo }) => {
-    const navigate = useNavigate();
+  const navigate = useNavigate();
 
-    const handleButtonClick = (action: string) => {
-        if (action === 'home') {
-            navigate('/');
-        } else if (action === 'saveUser') {
-            let info = localStorage.getItem('userInfo');
-            if (!info) return;
-            let parsedInfo = JSON.parse(info) as UserInfo;
-            if (parsedInfo) parsedInfo.id = parsedInfo.id.toString();
+  const handleButtonClick = (action: string) => {
+    if (action === "home") {
+      navigate("/");
+    } else if (action === "saveUser") {
+      let info = localStorage.getItem("userInfo");
+      if (!info) return;
+      let parsedInfo = JSON.parse(info) as UserInfo;
+      if (parsedInfo) parsedInfo.id = parsedInfo.id.toString();
 
-            axios
-                .post('http://apollo-lb-220895166.ap-northeast-2.elb.amazonaws.com/api/save/user', JSON.stringify(parsedInfo), {
-                    headers: { 'Content-Type': `application/json` },
-                })
-                .then((response) => {
-                    console.log('성공띠');
-                })
-                .catch((error) => {
-                    console.error('땡:', error);
-                });
-        }
-    };
+      axios
+        .post(
+          "http://apollo-lb-220895166.ap-northeast-2.elb.amazonaws.com/api/save/user",
+          JSON.stringify(parsedInfo),
+          {
+            headers: { "Content-Type": `application/json` },
+          }
+        )
+        .then((response) => {
+          console.log("성공띠");
+          navigate("/");
+        })
+        .catch((error) => {
+          console.error("땡:", error);
+        });
+    }
+  };
 
-    return (
-        <div>
-            <Button onClick={() => handleButtonClick('saveUser')}>눌러줘잉~</Button>
-        </div>
-    );
+  return (
+    <div>
+      <Button onClick={() => handleButtonClick("saveUser")}>눌러줘잉~</Button>
+    </div>
+  );
 };
 
 export default Wait;

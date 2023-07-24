f14a2ed2abc0c0b5d141a0bc5c8b7988673783a2
kurtyoon <akakertcobain@naver.com>
2023-07-24 15:22:07 +0900

    Refactor: user 생성 주석 삭제

--- src/apis/GetAuthenticationService.ts
+++ src/apis/GetAuthenticationService.ts
@@ -1,14 +1,5 @@
 import { apiClient } from "./ApiClient";
 
 export const getAuthenticationService = (code: String) => {
-  return apiClient.post(
-    `/api/authenticate`,
-    {
-      auth: {
-        username: "dlawjddn",
-        password: "dlawjddn",
-      },
-    },
-    { params: { code } }
-  );
+  return apiClient.post(`/api/authenticate`, {}, { params: { code } });
 };

3817c88c23477ca3bfe78f3e6069b626425e7b82
minwoo0419 <ghdl464@naver.com>
2023-07-29 13:44:08 +0900

    feat: 자동 로그인,회원가입

--- src/pages/MainPage/components/Signup.tsx
+++ src/pages/MainPage/components/Signup.tsx
@@ -1,6 +1,4 @@
 import { useEffect, useCallback } from "react";
-import github from "../../../assets/images/github_logo.png";
-import "../../../assets/css/button.css";
 import { getAuthenticationService } from "../../../apis/UserService";
 import { useNavigate } from "react-router-dom";
 
@@ -9,9 +7,9 @@ export function handleLogout() {
   window.location.href = "/";
 }
 
-function LoginButton() {
+function Signup() {
   const navigate = useNavigate();
-  const handleLogin = () => {
+  const handleSignin = () => {
     const githubAuthUrl = `https://github.com/login/oauth/authorize?client_id=7600733c0c5ed7849ce6`;
     window.location.href = githubAuthUrl;
   };
@@ -26,7 +24,7 @@ function LoginButton() {
         .then((res) => {
           console.log(res);
           localStorage.setItem("userInfo", JSON.stringify(res.data));
-          navigate("/wait", { state: { action: 'userSignIn' } });
+          navigate("/wait", { state: { action: 'userSignUp' } });
         })
         .catch((err) => {
           console.log("here");
@@ -42,11 +40,8 @@ function LoginButton() {
   }, [handleCallback]);
 
   return (
-    <button className="login" onClick={handleLogin}>
-      <img src={github} className="github" alt="github" />
-      Log in with GitHub
-    </button>
+    <p style={{color: "white" ,margin: "10px", cursor: "pointer" }} onClick={handleSignin}>signup</p>
   );
 }
 
-export default LoginButton;
+export default Signup;

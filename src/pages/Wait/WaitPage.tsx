b34d9f4aa10747655e56df1fdc3bff42592f90e7
minwoo0419 <ghdl464@naver.com>
2023-08-03 17:34:36 +0900

    로그인/회원가입 시 alert추가

--- src/pages/Wait/WaitPage.tsx
+++ src/pages/Wait/WaitPage.tsx
@@ -3,21 +3,42 @@ import {
   getUserSignInService,
   getUserSignUpService,
 } from "../../apis/UserService";
-import { useCallback, useEffect } from "react";
+import { useCallback, useEffect, useState } from "react";
 import FadeLoader from "react-spinners/FadeLoader";
 import { apiClient } from "../../apis/ApiClient";
 import useAuth from "../../hooks/authhook";
 import { defaultAuth } from "../../contexts/AuthContext";
 import useToken from "../../hooks/tokenhook";
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import styled from "@emotion/styled";
+
+
+export const StyledToastContainer = styled(ToastContainer)`
+
+  .Toastify__toast {
+    background-color: gray;
+    color: white;
+    font-weight: bold;
+    border-radius: 10px;
+    font-size: 30px;
+    height: 100px;
+    width: 300px;
+  }
+`
 
 const Wait = () => {
   const navigate = useNavigate();
   const action = localStorage.getItem("action");
+  const [isFinish, SetFinish] = useState(false);
   const {auth, setAuth} = useAuth();
   const {setToken} = useToken();
   const handleLogin = useCallback(() => {
     let userLogin = auth.login;
     let userId = auth.id;
+    const notify = (message:string) => toast(message, {
+      onClose: () => navigate("/")
+    });
     if (action === "userSignUp") {
       getUserSignUpService(auth)
         .then((response) => {
@@ -25,10 +46,15 @@ const Wait = () => {
           console.log(response);
           localStorage.removeItem("action");
           setAuth(defaultAuth);
-          navigate("/");
+          SetFinish(true);
+          notify("회원가입이 완료되었습니다.");
           })
         .catch((error) => {
           console.log("error: ", error);
+          alert("에러가 발생했습니다: " + error.response.data);
+          setTimeout(() => {
+            navigate("/");
+          }, 3000);
         });
     } else if (action === "userSignIn") {
       getUserSignInService(userLogin, userId)
@@ -40,12 +66,16 @@ const Wait = () => {
           ] = `${response.data.result.grantType} ${response.data.result.accessToken}`;
           localStorage.removeItem("action");
           setToken(response.data.result.accessToken);
-          navigate("/");
+          notify("로그인이 완료되었습니다.");
           return response.data;
         })
-          .catch((e) => {
-            console.log(e.response.data);
-            return "error";
+          .catch((error) => {
+            console.log(error.response.data);
+            alert("에러가 발생했습니다: " + error.response.data);
+            setTimeout(() => {
+              navigate("/");
+            }, 3000);
+            return error;
         });
 
     }
@@ -53,8 +83,20 @@ const Wait = () => {
   useEffect(() => {
     handleLogin();
   }, [handleLogin, action]);
+
   return (
-    <div className="contentWrap">
+    <>
+    {isFinish ? (
+      <StyledToastContainer
+        position="top-center"
+        limit={1}
+        closeOnClick
+        autoClose={5000}
+        hideProgressBar
+        pauseOnHover
+      />
+    ) : (
+      <div className="contentWrap">
       <div
         style={{
           position: "fixed",
@@ -72,6 +114,8 @@ const Wait = () => {
         />
       </div>
     </div>
+    )}
+  </>
   );
 };
 

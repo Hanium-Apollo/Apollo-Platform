4a0a22013829c6ee40bc783736385afc73f13915
minwoo0419 <ghdl464@naver.com>
2023-08-06 18:41:26 +0900

    fix : 회원가입 2번오류 수정

--- src/pages/MainPage/components/Signup.tsx
+++ src/pages/MainPage/components/Signup.tsx
@@ -8,7 +8,6 @@ export const Signup = () => {
     const githubAuthUrl = `https://github.com/login/oauth/authorize?client_id=7600733c0c5ed7849ce6`;
     window.location.href = githubAuthUrl;
     localStorage.setItem("action", "userSignUp");
-    navigate("/wait");
   };
 
   return (

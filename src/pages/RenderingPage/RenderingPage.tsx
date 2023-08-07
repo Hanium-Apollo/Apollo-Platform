d4f08980aa0a25ff8032f4848e227731b8a9603a
kurtyoon <akakertcobain@naver.com>
2023-08-07 15:35:11 +0900

    Fix: 렌더링 페이지 배포 대기 화면에서 우주선 -> 로딩 애니메이션으로 변경

--- src/pages/RenderingPage/RenderingPage.tsx
+++ src/pages/RenderingPage/RenderingPage.tsx
@@ -1,6 +1,7 @@
 import { useEffect, useCallback } from "react";
 import "../../assets/css/RenderingPage.css";
 import { useLocation, useNavigate } from "react-router-dom";
+import FadeLoader from "react-spinners/FadeLoader";
 import {
   clientRepoCreateService,
   serverRepoCreateService,
@@ -45,7 +46,17 @@ function Rendering() {
   return (
     <div className="loading-container">
       <p className="rmessage">WAIT A MINUTE !</p>
-      <div className="moving-image"></div>
+      {/* <div className="moving-image"></div> */}
+      <div
+        style={{
+          position: "fixed",
+          top: "50%",
+          left: "50%",
+          transform: "translate(-50%, -50%)",
+        }}
+      >
+        <FadeLoader color="white" height={15} width={5} radius={2} margin={2} />
+      </div>
     </div>
   );
 }

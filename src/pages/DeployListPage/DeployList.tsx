3384e492bb44f54a5a0954f4496e19ee95d0f5d4
minwoo0419 <ghdl464@naver.com>
2023-08-09 00:33:20 +0900

    deploylist, delete 구현

--- src/pages/DeployListPage/DeployList.tsx
+++ src/pages/DeployListPage/DeployList.tsx
@@ -1,30 +1,73 @@
 import { useNavigate } from "react-router-dom";
 import "../../assets/css/deploy.css";
 import Button from "../../components/button/Button";
+import { useCallback, useEffect, useState } from "react";
+import {
+  clientRepoDeleteService,
+  getClientDeployListService,
+  getServerDeployListService,
+  serverRepoDeleteService,
+} from "../../apis/RepoService";
+import { UserInfo } from "../../apis/UserServiceType";
+type deployData = {
+  repoName: string;
+  type: string;
+};
+type ListItemProps = {
+  deploylist: deployData[];
+  type: string;
+};
 
-function ListItem(props: any) {
+function ListItem({ repoName, type }: deployData) {
   const navigate = useNavigate();
   const handleSubmit = () => {
-    const repoName = props.repoName;
-    console.log(props.repoName);
+    console.log(repoName);
     navigate("/monitor", { state: { repoName } });
+  };
+  const handleClick = () => {
+    if (type === "client") {
+      clientRepoDeleteService(repoName)
+        .then((response) => {
+          console.log(response.data);
+        })
+        .catch((error) => {
+          console.error("Error fetching data:", error);
+        });
+    } else if (type === "server") {
+      serverRepoDeleteService(repoName)
+        .then((response) => {
+          console.log(response.data);
+        })
+        .catch((error) => {
+          console.error("Error fetching data:", error);
+        });
+    }
+    navigate("/deploylist");
   };
   return (
     <div
       style={{ display: "flex", alignItems: "center", marginBottom: "15px" }}
     >
-      <li className="list">{props.repoName}</li>
-      <button className="selectbtn" onClick={handleSubmit}>
+      <li className="list">{repoName}</li>
+      type === "client" ? (
+      <button className="selectbtn" onClick={() => handleClick()}>
+        삭제
+      </button>
+      ) : (
+      <button className="deploybtn1" onClick={handleSubmit}>
         모니터링
       </button>
+      <button className="deploybtn2" onClick={() => handleClick()}>
+        삭제
+      </button>
+      )
     </div>
   );
 }
 
-function NumberList(props: any) {
-  const numbers = props.numbers;
-  const listItems = numbers.map((number: number) => (
-    <ListItem key={number.toString()} value={number} />
+function NumberList({ deploylist, type }: ListItemProps) {
+  const listItems = deploylist.map((item, index) => (
+    <ListItem key={index.toString()} repoName={item.repoName} type={type} />
   ));
   return (
     <div className="deploylist">
@@ -33,13 +76,53 @@ function NumberList(props: any) {
   );
 }
 
-function DeployList(props: any) {
-  const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11];
+function DeployList() {
+  const [ServerData, setServerData] = useState([]);
+  const [ClientData, setClientData] = useState([]);
+  const getDeploy = useCallback(() => {
+    let info = localStorage.getItem("userInfo");
+    let parsedInfo = info ? (JSON.parse(info) as UserInfo) : null;
+    let accessToken = localStorage.getItem("token");
+    if (!parsedInfo) return;
+    let userLogin = parsedInfo.login;
+    if (accessToken && userLogin) {
+      getServerDeployListService(userLogin)
+        .then((response) => {
+          console.log(response.data);
+          setServerData(response.data);
+        })
+        .catch((error) => {
+          console.error("Error fetching data:", error);
+        });
+      getClientDeployListService(userLogin)
+        .then((response) => {
+          console.log(response.data);
+          setClientData(response.data);
+        })
+        .catch((error) => {
+          console.error("Error fetching data:", error);
+        });
+    }
+  }, []);
+
+  useEffect(() => {
+    getDeploy();
+  }, [getDeploy]);
   return (
     <div className="deploy">
-      <div className="name">배포 중인 서비스</div>
-      <NumberList numbers={numbers} />
-      <Button css={"fhomebtn"} text={"home"} />
+      <div>
+        <div className="name">배포 중 client</div>
+        {ClientData !== null && (
+          <NumberList deploylist={ClientData} type="client" />
+        )}
+      </div>
+      <div>
+        <div className="name">배포 중 server</div>
+        {ServerData !== null && (
+          <NumberList deploylist={ServerData} type="server" />
+        )}
+        <Button css={"fhomebtn"} text={"home"} />
+      </div>
     </div>
   );
 }

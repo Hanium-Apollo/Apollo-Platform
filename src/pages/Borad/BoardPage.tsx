a051f2ae3214e35cfdf8b0b36f5ec93def765892
cho1n <cjaewon0217@naver.com>
2023-08-18 10:48:38 +0900

    Feat: Markdown 적용

--- src/pages/Borad/BoardPage.tsx
+++ src/pages/Borad/BoardPage.tsx
@@ -1,11 +1,30 @@
-import React from "react";
-import { css } from "@emotion/react";
+import React, { useState } from "react";
+import MDEditor from "@uiw/react-md-editor";
 import styled from "@emotion/styled";
 
+const ExampleBox = styled.div`
+  margin-top: 20px;
+  font-size: 14px;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+  top: 50%;
+  position: absolute;
+`;
+
 export const Board = () => {
+  const [value, setValue] = useState("");
+
+  const handleEditorChange = (newValue: string | undefined) => {
+    if (typeof newValue === "string") {
+      setValue(newValue);
+    }
+  };
+
   return (
-    <div>
-      <h1>Board</h1>
-    </div>
+    <ExampleBox>
+      <MDEditor value={value} onChange={handleEditorChange} preview="edit"/>
+      <MDEditor.Markdown source={value} style={{ whiteSpace: "pre-wrap" }} />
+    </ExampleBox>
   );
 };
